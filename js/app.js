254213ff3dcc0e9799cfe3a8c74d8390cd3a3132
Sasha <htmlfau@gmail.com>
2017-03-04 13:24:14 +0200

    lesson_35

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+
+
+$(document).ready(function(){
+    $('.portfolio-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true
+    });
+});
